a8f39509f54b159ceb1ab8ce03f07d8e5e466f71
Saurabh <saurabhkandale07@gmail.com>
2022-11-23 16:32:15 +0530

    blc dep added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
+import {useState, useEffect} from 'react';
+import {collection,getDocs} from 'firebase/firestore';
+import {db} from './firebase-config';
+import Form from './Components/Form/Form';
+import {Routes,Route} from 'react-router-dom'
+import Intro from './Components/Intro';
 
 function App() {
+
+  const [users,setUsers]=useState([]);
+  const userDataCollection=collection(db,"users");
+
+  useEffect(()=>{
+    const getUsers=async ()=>{
+      const data=await getDocs(userDataCollection);
+      setUsers(data.docs.map((doc)=>({...doc.data(),id :doc.id})));
+    }
+
+    getUsers();
+    console.log(users);
+  },[])
+
   return (
-    <div className="App">
-      
-    </div>
+      <Routes>
+        <Route path='/' element={<Intro/>}></Route>
+        <Route path='/userLogin' element={<Form/>}></Route>
+      </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+//wire income dinosaur nothing turn easily wave accident poem east deer crowd
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
